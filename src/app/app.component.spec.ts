fb8a29bb06977e964ba194db07ea641bb1c68975
dylbud <dylbudbragg@gmail.com>
2021-10-22 13:34:52 -0700

    renamed project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-advancement'`, () => {
+  it(`should have as title 'book-finder'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("angular-advancement");
+    expect(app.title).toEqual("book-finder");
   });
 
   it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector(".content span").textContent).toContain("angular-advancement app is running!");
+    expect(compiled.querySelector(".content span").textContent).toContain("book-finder app is running!");
   });
 });
