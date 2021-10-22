3a95a6bda742d9b4f51360f70c755886043a97bd
dylbud <dylbudbragg@gmail.com>
2021-10-22 13:21:05 -0700

    more formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BookListComponent } from './book-list/book-list.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BookListComponent } from "./book-list/book-list.component";
 
-const routes: Routes = [
-  {path: 'book-list', component: BookListComponent}
-];
+const routes: Routes = [{ path: "book-list", component: BookListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
